674101015aeaf3865511881c93a543dc989261f3
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-17 07:53:51 +0000

    Deploying to gh-pages from @ b-on-g/leaderboard@984c3110267b498c90a3728ebe6f730695d57255 🚀

--- node.js
+++ node.js
@@ -11358,7 +11358,14 @@ var $;
                 if (unit instanceof $giper_baza_auth_pass)
                     continue;
                 if (this.lord_tier(unit.lord()) < unit.tier_min()) {
-                    return this.$.$mol_fail(new Error('Too low Tier'));
+                    this.$.$mol_log3_warn({
+                        message: 'Too low Tier',
+                        tier_min: unit.tier_min().toString(2),
+                        tier_actual: this.lord_tier(unit.lord()).toString(2),
+                        hint: 'Relax. Unit is skipped.',
+                        place: `${this}.diff_apply()`,
+                    });
+                    continue;
                 }
                 const lord_pass = this.lord_pass(unit.lord()) ?? passes.get(unit.lord().str);
                 if (!lord_pass)
@@ -11796,13 +11803,14 @@ var $;
                 const wide = Boolean(land.link().area().str);
                 return $mol_array_chunks(hashes, $giper_baza_unit_seal_limit).map(async (hashes) => {
                     const seal = $giper_baza_unit_seal.make(hashes.length, wide);
-                    seal.time_tick(this.faces.tick().time_tick);
                     seal.lord(auth.pass().lord());
                     seal.hash_list(hashes);
                     seal._land = this;
-                    const shot = seal.shot().mix(this.link());
                     do {
-                        seal.sign(await auth.signer().sign(shot));
+                        seal.time_tick(this.faces.tick().time_tick);
+                        const sens = seal.shot().mix(this.link());
+                        const sign = await auth.signer().sign(sens);
+                        seal.sign(sign);
                     } while (seal.rate_min() > rate);
                     return seal;
                 });
@@ -13103,9 +13111,8 @@ var $;
                 return this._hash_list = list;
             }
         }
-        _shot;
         shot() {
-            return this._shot ?? (this._shot = $giper_baza_link.hash_bin(new Uint8Array(this.buffer, this.byteOffset, this.byteLength - 64)));
+            return $giper_baza_link.hash_bin(new Uint8Array(this.buffer, this.byteOffset, this.byteLength - 64));
         }
         sign(next) {
             const buf = new Uint8Array(this.buffer, this.byteOffset + this.byteLength - 64, 64);
@@ -13114,7 +13121,7 @@ var $;
             return buf;
         }
         work() {
-            let int = this.uint32(this.byteLength - 64);
+            let int = new Uint32Array(this.hash().toBin().buffer)[0];
             let count = 0;
             while (int & 1) {
                 int >>>= 1;
@@ -15286,7 +15293,7 @@ var $;
                     message: error.message ?? '',
                     origin: msg.origin(),
                     address: msg.address(),
-                    casue: error.cause,
+                    cause: error.cause,
                     stack: error.stack,
                 });
                 $mol_wire_sync(res).writeHead(500, error.name || 'Server Error');
@@ -15315,7 +15322,7 @@ var $;
                     message: error.message ?? '',
                     origin: upgrade.origin(),
                     address: upgrade.address(),
-                    casue: error.cause,
+                    cause: error.cause,
                     stack: error.stack,
                 });
                 socket.end();
@@ -15341,7 +15348,7 @@ var $;
                         message: error.message ?? '',
                         origin: upgrade.origin(),
                         address: upgrade.address(),
-                        casue: error.cause,
+                        cause: error.cause,
                         stack: error.stack,
                     });
                     return;
@@ -15456,7 +15463,7 @@ var $;
                     message: error.message ?? '',
                     origin: upgrade.origin(),
                     address: upgrade.address(),
-                    casue: error.cause,
+                    cause: error.cause,
                     stack: error.stack,
                 });
                 sock.end();
--- web.d.ts
+++ web.d.ts
@@ -3364,7 +3364,6 @@ declare namespace $ {
         hash_item(index: number, next?: $giper_baza_link): $giper_baza_link;
         _hash_list: readonly $giper_baza_link[];
         hash_list(next?: $giper_baza_link[]): $giper_baza_link[];
-        _shot: $giper_baza_link;
         shot(): $giper_baza_link;
         sign(next?: Uint8Array<ArrayBuffer>): Uint8Array<ArrayBuffer>;
         work(): number;
@@ -7437,7 +7436,7 @@ declare namespace $ {
 //# sourceMappingURL=security.view.tree.d.ts.map
 declare namespace $.$$ {
     class $giper_baza_link_chip extends $.$giper_baza_link_chip {
-        subj(): $giper_baza_flex_subj;
+        subj(): $giper_baza_flex_subj | null;
         meta(): $giper_baza_flex_meta | null;
         icon(): string;
         title(): string;
@@ -7460,6 +7459,9 @@ declare namespace $ {
 }
 
 //# sourceMappingURL=chip.view.tree.d.ts.map
+declare namespace $.$$ {
+}
+
 declare namespace $ {
 }
 
@@ -10090,7 +10092,7 @@ declare namespace $.$$ {
         peer_id(lord: $giper_baza_link): string;
         gift_tier(lord: $giper_baza_link, next?: keyof typeof $giper_baza_rank_tier): keyof typeof $giper_baza_rank_tier;
         gift_rate(lord: $giper_baza_link, next?: string): string;
-        add_commit(): void;
+        add_commit(): undefined;
         tier_options(): {
             deny: string;
             read: string;
@@ -12118,7 +12120,7 @@ declare namespace $.$$ {
 
 declare namespace $.$$ {
     class $giper_baza_app extends $.$giper_baza_app {
-        auto(): void;
+        auto(): never[];
     }
 }
 
