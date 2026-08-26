58820e9e53895e9ce0300a30d0c9e9f950af75f5
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-08-26 07:44:26 +0000

    Deploying to gh-pages from @ b-on-g/leaderboard@984c3110267b498c90a3728ebe6f730695d57255 🚀

--- web.js
+++ web.js
@@ -27463,12 +27463,23 @@ var $;
 
 ;
 	($.$giper_baza_app_stat_list) = class $giper_baza_app_stat_list extends ($.$mol_book2_catalog) {
+		peer_urls(id){
+			return (this.peer_home(id).urls());
+		}
 		peer_home(id){
 			const obj = new this.$.$giper_baza_app_home();
 			return obj;
 		}
 		peer_uptime(id){
 			return (this.Spread(id).uptime());
+		}
+		peer_uri(id){
+			return "";
+		}
+		Menu_link_avatar(id){
+			const obj = new this.$.$mol_avatar();
+			(obj.id) = () => ((this.peer_uri(id)));
+			return obj;
 		}
 		Menu_link_status(id){
 			const obj = new this.$.$mol_view();
@@ -27488,10 +27499,15 @@ var $;
 			return obj;
 		}
 		menu_link_content(id){
-			return [(this.Menu_link_title(id)), (this.Menu_link_status(id))];
+			return [
+				(this.Menu_link_avatar(id)), 
+				(this.Menu_link_title(id)), 
+				(this.Menu_link_status(id))
+			];
 		}
 	};
 	($mol_mem_key(($.$giper_baza_app_stat_list.prototype), "peer_home"));
+	($mol_mem_key(($.$giper_baza_app_stat_list.prototype), "Menu_link_avatar"));
 	($mol_mem_key(($.$giper_baza_app_stat_list.prototype), "Menu_link_status"));
 	($mol_mem_key(($.$giper_baza_app_stat_list.prototype), "Spread"));
 
@@ -27529,6 +27545,9 @@ var $;
             peer_home(id) {
                 return this.$.$giper_baza_glob.Pawn(new $giper_baza_link(id), $giper_baza_app_home);
             }
+            peer_uri(id) {
+                return this.peer_urls(id)[0] ?? '';
+            }
         }
         __decorate([
             $mol_mem
@@ -27539,6 +27558,9 @@ var $;
         __decorate([
             $mol_mem_key
         ], $giper_baza_app_stat_list.prototype, "peer_home", null);
+        __decorate([
+            $mol_mem_key
+        ], $giper_baza_app_stat_list.prototype, "peer_uri", null);
         $$.$giper_baza_app_stat_list = $giper_baza_app_stat_list;
     })($$ = $.$$ || ($.$$ = {}));
 })($ || ($ = {}));
@@ -27552,12 +27574,17 @@ var $;
         $mol_style_define($giper_baza_app_stat_list, {
             Menu: {
                 flex: {
-                    basis: '20rem',
+                    basis: '25rem',
                 },
             },
             Menu_link: {
                 justify: {
                     content: 'space-between',
+                },
+            },
+            Menu_link_title: {
+                flex: {
+                    grow: 1,
                 },
             },
         });
