2b15cb9564e55ffa3243521fee7e12f30c2538c3
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-08-27 18:06:35 +0000

    Deploying to gh-pages from @ b-on-g/leaderboard@984c3110267b498c90a3728ebe6f730695d57255 🚀

--- web.mjs
+++ web.mjs
@@ -27437,7 +27437,7 @@ var $;
     (function ($$) {
         $mol_style_define($giper_baza_app_stat_page, {
             flex: {
-                basis: `40rem`,
+                basis: `100%`,
                 grow: 1,
             },
             Main: {
@@ -27574,7 +27574,7 @@ var $;
         $mol_style_define($giper_baza_app_stat_list, {
             Menu: {
                 flex: {
-                    basis: '25rem',
+                    basis: '30rem',
                 },
             },
             Menu_link: {
